14f1bc73c7558e794580a7876b7fcdfe99d70082
Amine <hilalyamine@gmail.com>
2024-07-12 14:31:09 -0700

    Reset repository

--- internal/schema/transformer.go
+++
@@ -1,9 +0,0 @@
-package schema
-
-import extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-
-// Transformer is an interface for transforming raw objects into JSONSchemaProps
-type Transformer interface {
-	// Transform takes a raw object and returns a JSONSchemaProps
-	Transform(raw interface{}) (*extv1.JSONSchemaProps, error)
-}
