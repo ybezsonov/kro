b37e91fc358c8f56660afc298b9ec5e8c73fad66
Greg Haynes <greg@greghaynes.net>
2024-11-14 00:31:17 +0000

    Remove duplicate api import
    Signed-off-by: Greg Haynes <greg@greghaynes.net>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -31,7 +31,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	"github.com/awslabs/kro/api/v1alpha1"
 	xv1alpha1 "github.com/awslabs/kro/api/v1alpha1"
 	kroclient "github.com/awslabs/kro/internal/client"
 	resourcegroupctrl "github.com/awslabs/kro/internal/controller/resourcegroup"
@@ -178,7 +177,7 @@ func main() {
 		ctrlrtcontroller.Options{
 			MaxConcurrentReconciles: resourceGroupConcurrentReconciles,
 		},
-	).Complete(reconcile.AsReconciler[*v1alpha1.ResourceGroup](mgr.GetClient(), reconciler))
+	).Complete(reconcile.AsReconciler[*xv1alpha1.ResourceGroup](mgr.GetClient(), reconciler))
 	if err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "ResourceGroup")
 		os.Exit(1)
