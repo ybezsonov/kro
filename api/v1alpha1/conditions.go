c8ec4b00ea8d0cbecaeab41d57fda7044b5b7be1
Venkata S S K M Chaitanya <16039325+vmanikes@users.noreply.github.com>
2025-02-17 16:51:22 -0700

    Use slices package based lookup in HasCondition (#298)

--- api/v1alpha1/conditions.go
+++ api/v1alpha1/conditions.go
@@ -15,13 +15,14 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"slices"
 )
 
 // ConditionType is a type of condition for a resource.
 type ConditionType string
 
 const (
-	// ResourceGraphDefinitionConditionTypeGraphSynced indicates the state of the directed
+	// ResourceGraphDefinitionConditionTypeGraphVerified indicates the state of the directed
 	// acyclic graph (DAG) that kro uses to manage the resources in a
 	// ResourceGraphDefinition.
 	ResourceGraphDefinitionConditionTypeGraphVerified ConditionType = "GraphVerified"
@@ -39,13 +40,13 @@ const (
 const (
 	InstanceConditionTypeReady ConditionType = "Ready"
 
-	// Creating Deleting Migrating
+	// InstanceConditionTypeProgressing used for Creating Deleting Migrating
 	InstanceConditionTypeProgressing ConditionType = "Progressing"
 
-	// Unexpected situation, behaviour, need human intervention
+	// InstanceConditionTypeDegraded used in unexpected situation, behaviour, need human intervention
 	InstanceConditionTypeDegraded ConditionType = "Degraded"
 
-	// Something is wrong but i'm gonna try again
+	// InstanceConditionTypeError used in something is wrong but i'm going to try again
 	InstanceConditionTypeError ConditionType = "Error"
 )
 
@@ -63,7 +64,7 @@ type Condition struct {
 	// The reason for the condition's last transition.
 	// +optional
 	Reason *string `json:"reason,omitempty"`
-	// A human readable message indicating details about the transition.
+	// A human-readable message indicating details about the transition.
 	// +optional
 	Message *string `json:"message,omitempty"`
 	// observedGeneration represents the .metadata.generation that the condition was set based upon.
@@ -85,15 +86,6 @@ func NewCondition(t ConditionType, status metav1.ConditionStatus, reason, messag
 	}
 }
 
-func GetCondition(conditions []Condition, t ConditionType) *Condition {
-	for _, c := range conditions {
-		if c.Type == t {
-			return &c
-		}
-	}
-	return nil
-}
-
 func SetCondition(conditions []Condition, condition Condition) []Condition {
 	for i, c := range conditions {
 		if c.Type == condition.Type {
@@ -105,5 +97,7 @@ func SetCondition(conditions []Condition, condition Condition) []Condition {
 }
 
 func HasCondition(conditions []Condition, t ConditionType) bool {
-	return GetCondition(conditions, t) != nil
+	return slices.ContainsFunc(conditions, func(c Condition) bool {
+		return c.Type == t
+	})
 }
